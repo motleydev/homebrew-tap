048fa2961ba94549e4ab39c898e4a242ea8d6331
Jesse Martin <themotleydev@gmail.com>
2026-02-13 07:44:36 -0500

    Add gitsneak formula

--- Formula/gitsneak.rb
+++ Formula/gitsneak.rb
@@ -1,6 +1,6 @@
 class Gitsneak < Formula
   desc "Analyze organizational involvement in GitHub repositories"
-  homepage "https://github.com/jesseops/gitsneak"
+  homepage "https://github.com/motleydev/gitsneak"
   url "https://registry.npmjs.org/gitsneak/-/gitsneak-1.0.0.tgz"
   sha256 "eea0738f57a90cf2b28e851e905e091779a7351b618d17d93c6dd3d4becfd5a4"
   license "MIT"
